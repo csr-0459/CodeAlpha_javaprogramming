6915f968452700bd3700c2e86cdb9879b3303425
csr-0459 <chakri4439@gmail.com>
2025-02-04 10:20:16 +0530

    Add files via upload
    Student Grade Tracker Develop a program that allows a teacher to enter students' grades and compute their average, highest, and lowest scores. You can use arrays or ArrayLists to store the student data.

---
+++ CodeAlpha_1.java
@@ -0,0 +1,50 @@
+/* Student Grade Tracker Develop a program that allows a teacher to 
+enter students' grades and compute their average, highest, and lowest scores. 
+You can use arrays or ArrayLists to store the student data.*/
+
+
+
+import java.util.*;
+import java.util.Collections;
+class CodeAlpha_1
+{
+    static public void main(String args[])
+    {
+        Scanner ob=new Scanner(System.in);
+        int sum=0,highest,lowest,n,i;
+        float avg;
+        System.out.println("Enter number of students : ");
+        n=ob.nextInt();
+        int students_grade[]=new int[n];
+
+        System.out.println("Enter the suudents grade : ");
+        for(i=0;i<n;i++)
+        {
+            students_grade[i]=ob.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            sum+=students_grade[i];
+        }
+        highest=students_grade[0];
+        lowest=students_grade[0];
+        for(i=0;i<n;i++)
+        {
+            if(lowest>students_grade[i])
+            {
+                lowest=students_grade[i];
+            }
+            if(highest<students_grade[i])
+            {
+                highest=students_grade[i];
+            }
+        }
+        System.out.println("Lowest grade : "+lowest+"\nHighest grade : "+highest);
+        avg=sum/n;
+        System.out.println("Average of grades : "+avg);
+
+
+
+
+    }
+}
